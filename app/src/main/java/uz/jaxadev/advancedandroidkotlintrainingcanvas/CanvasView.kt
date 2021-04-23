6af684552ef8eedbc4483a077dac45bf4b7a34ce
mannonov <jahongir.sh.mannonov@gmail.com>
2021-04-23 15:50:00 +0500

    drawing added

--- app/src/main/java/uz/jaxadev/advancedandroidkotlintrainingcanvas/CanvasView.kt
+++ app/src/main/java/uz/jaxadev/advancedandroidkotlintrainingcanvas/CanvasView.kt
@@ -1,9 +1,15 @@
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 import uz.jaxadev.advancedandroidkotlintrainingcanvas.R
+
+private const val STROKE_WIDTH = 12f
 
 class CanvasView(context: Context) : View(context) {
 
@@ -11,6 +17,18 @@ class CanvasView(context: Context) : View(context) {
     private lateinit var extraBitmap: Bitmap
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    private var path = Path()
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -31,5 +49,52 @@ class CanvasView(context: Context) : View(context) {
         super.onDraw(canvas)
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
     }
+
+
+    private val paint = Paint().apply {
+        color = drawColor
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+    private fun touchStart() {
+            path.reset()
+            path.moveTo(motionTouchEventX, motionTouchEventY)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
+
+    private fun touchUp() {
+        path.reset()
+    }
+
 
 }
--- app/src/main/java/uz/jaxadev/advancedandroidkotlintrainingcanvas/MainActivity.kt
+++ app/src/main/java/uz/jaxadev/advancedandroidkotlintrainingcanvas/MainActivity.kt
@@ -1,11 +1,24 @@
 package uz.jaxadev.advancedandroidkotlintrainingcanvas
 
+import CanvasView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.jaxadev.advancedandroidkotlintrainingcanvas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val canvasView = CanvasView(this)
+
+        binding.rootLayout.addView(canvasView)
+
+
     }
 }
